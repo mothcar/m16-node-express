29dcaded75882503df993de435b3838bd43621fd
Paul <mothcar@gmail.com>
2022-12-29 05:19:37 +0000

    first set

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const mongoose = require('mongoose');
+const url = "mongodb://admin:admin@svc.gksl2.cloudtype.app:31177/?authMechanism=DEFAULT"
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, {useNewUrlParser: true}, (err)=>{
+  if(err) console.log(err)
+  else console.log('Connected... successfully....')
+})
+const db = mongoose.connection
+db.on('error', (error)=>console.log(error))
+db.once('open', ()=>console.log('DB connected..'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
